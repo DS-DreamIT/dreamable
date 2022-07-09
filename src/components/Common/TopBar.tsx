9aa6084aefe5829e0253fcba03b57195bd483ecc
sky <shn7446@duksung.ac.kr>
2022-07-10 01:14:27 +0900

    refactor: ResultPage & MoodCard - ResultPage : css 변수 이름 변경, MoodCard 모듈화 - MoodCard : 현재는 꿈 분석 단어, 꿈 단어도 추가할 방안 모색 - TopBar : 날짜 정보 props

--- src/components/Common/TopBar.tsx
+++ src/components/Common/TopBar.tsx
@@ -1,24 +1,31 @@
 import React, {useCallback} from 'react'
-import {StyleSheet, Image, TouchableOpacity} from 'react-native'
+import {StyleSheet, View, Image, Text, TouchableOpacity} from 'react-native'
 
 // @ts-ignore
-const TopBar = ({navigation}) => {
+const TopBar = ({navigation, date}) => {
   const goBack = useCallback(() => navigation.pop(), []) // 오류
 
   return (
     <TouchableOpacity onPress={goBack}>
-      <Image
-        style={[styles.back]}
-        source={require('../../assets/icons/arrow-back.png')}
-      />
+      <View style={styles.view}>
+        <Image source={require('../../assets/icons/arrow-back.png')} />
+        <Text style={styles.text}>{date}</Text>
+      </View>
     </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
-  back: {
+  view: {
     marginTop: 20,
     marginLeft: 13,
+    marginBottom: 14,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    color: '#fff',
   },
 })
 
