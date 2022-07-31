cc23a3c04e3148159216df3740e343e852b97717
sky <shn7446@duksung.ac.kr>
2022-08-01 00:22:53 +0900

    fix: prettier 오류 해결

--- src/pages/Dream/ShareDream.tsx
+++ src/pages/Dream/ShareDream.tsx
@@ -1,81 +1,84 @@
-import React, { useRef, useEffect } from 'react';
-import {View, Text, ImageBackground, Image, StyleSheet, SafeAreaView, PermissionsAndroid, Platform, TouchableHighlight} from 'react-native'
+import React, {useRef} from 'react'
+import {
+  View,
+  Text,
+  ImageBackground,
+  Image,
+  StyleSheet,
+  SafeAreaView,
+  PermissionsAndroid,
+  Platform,
+  TouchableHighlight,
+} from 'react-native'
 import TopBar from '../../components/Common/TopBar'
 import Share from 'react-native-share'
-import ViewShot from "react-native-view-shot"
-import CameraRoll from '@react-native-community/cameraroll';
+import ViewShot from 'react-native-view-shot'
+import CameraRoll from '@react-native-community/cameraroll'
 
-const title = 'Share your Dreams';
-const smallTitle = 'Personal Dream';
+const title = 'Share your Dreams'
+const smallTitle = 'Personal Dream'
 
 // @ts-ignore
 //const captureRef = useRef<any | undefined | null>()
 export default function ShareDream({navigation}) {
-    const captureRef = useRef(null)
-    const getPhotoUri = async (): Promise<string> => {
-      // @ts-ignore
-      const uri = await captureRef.current.capture();
-      console.log('Image saved to', uri);
-      return uri;
-    };
+  const captureRef = useRef(null)
+  const getPhotoUri = async (): Promise<string> => {
+    // @ts-ignore
+    const uri = await captureRef.current.capture()
+    console.log('Image saved to', uri)
+    return uri
+  }
 
-    useEffect(() => {
-      // @ts-ignore
-      captureRef.current.capture().then((uri) => {
-        console.log('do something with ', uri);
-      });
-    }, []);
-    
-// @ts-ignore
-    const onCapture = async (social: Share.social) => {
-      try {
-        const uri = await getPhotoUri();
+  // @ts-ignore
+  const onCapture = async (social: Share.social) => {
+    try {
+      const uri = await getPhotoUri()
 
-        const options = {
-          title: 'Share Title',
-          message: 'Share Message',
-          url: uri,
-          type: 'image/jpeg',
-        };
-
-        if (social === null) {
-          const result = await Share.open(options);
-          console.log('result with no social', result);
-        } else {
-          const result = await Share.shareSingle({
-            ...options,
-            social,
-          });
-          console.log(`result with social ${social}`, result);
-        }
-      } catch (e) {
-        console.log('화면 캡쳐 failed', e);
-      }
-    };
-    
-
-    const hasAndroidPermission = async () => {
-      const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
-
-      const hasPermission = await PermissionsAndroid.check(permission);
-      if (hasPermission) {
-        return true;
+      const options = {
+        title: 'Share Title',
+        message: 'Share Message',
+        url: uri,
+        type: 'image/jpeg',
       }
 
-      const status = await PermissionsAndroid.request(permission);
-      return status === 'granted';
-    };
-
-    const onSave = async () => {
-      if (Platform.OS === 'android' && !(await hasAndroidPermission())) {
-        return;
+      if (social === null) {
+        const result = await Share.open(options)
+        console.log('result with no social', result)
+      } else {
+        const result = await Share.shareSingle({
+          ...options,
+          social,
+        })
+        console.log(`result with social ${social}`, result)
       }
+    } catch (e) {
+      console.log('화면 캡쳐 failed', e)
+    }
+  }
 
-      const uri = await getPhotoUri();
-      const result = await CameraRoll.save(uri);
-      console.log('result', result);
-    };
-    
+  const hasAndroidPermission = async () => {
+    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+
+    const hasPermission = await PermissionsAndroid.check(permission)
+    if (hasPermission) {
+      return true
+    }
+
+    const status = await PermissionsAndroid.request(permission)
+    return status === 'granted'
+  }
+
+  const onSave = async () => {
+    console.log('here?')
+    if (Platform.OS === 'android' && !(await hasAndroidPermission())) {
+      return
+    }
+
+    const uri = await getPhotoUri()
+    const result = await CameraRoll.save(uri)
+    console.log('result', result)
+  }
+
   return (
     <SafeAreaView style={styles.flex}>
       <ImageBackground
@@ -85,8 +88,8 @@ export default function ShareDream({navigation}) {
         <View style={[styles.flex, styles.padding89]}>
           <Text style={styles.text}>{title}</Text>
           <View style={styles.sepline} />
-          
-          <ViewShot ref={captureRef} options={{ format: 'jpg', quality: 0.9 }}>
+
+          <ViewShot ref={captureRef} options={{format: 'jpg', quality: 1}}>
             <View>
               <View style={styles.bigbox}>
                 <Text style={styles.smalltitle}>{smallTitle}</Text>
@@ -98,28 +101,29 @@ export default function ShareDream({navigation}) {
               </View>
             </View>
           </ViewShot>
-          
+
           <View style={styles.sepunderline} />
           <View style={styles.iconstyle}>
-            <TouchableHighlight onPress={() => onCapture(Share.Social.FACEBOOK)}>
+            <TouchableHighlight
+              onPress={() => onCapture(Share.Social.FACEBOOK)}>
               <Image
                 source={require('../../assets/icons/share-facebook.png')}
               />
             </TouchableHighlight>
-            <TouchableHighlight onPress={() => onCapture(Share.Social.INSTAGRAM)}>
+            <TouchableHighlight
+              onPress={() => onCapture(Share.Social.INSTAGRAM)}>
               <Image
                 style={styles.icon}
                 source={require('../../assets/icons/share-instagram.png')}
               />
             </TouchableHighlight>
-            
-            <TouchableHighlight onPress={() => onSave}>
-                <Image
+
+            <TouchableHighlight onPress={() => onSave()}>
+              <Image
                 style={styles.icon}
                 source={require('../../assets/icons/share-save.png')}
-                />
+              />
             </TouchableHighlight>
-            
           </View>
         </View>
       </ImageBackground>
@@ -130,11 +134,11 @@ export default function ShareDream({navigation}) {
 const styles = StyleSheet.create({
   flex: {flex: 1},
   text: {
-    textAlign: 'center', 
-    fontSize: 40, 
-    color: '#ce93d8', 
+    textAlign: 'center',
+    fontSize: 40,
+    color: '#ce93d8',
     marginBottom: 10,
-    fontFamily: "Roboto-LightItalic"
+    fontFamily: 'Roboto-LightItalic',
   },
   padding89: {padding: 25, paddingLeft: 0, paddingRight: 0},
   sepline: {
@@ -175,4 +179,4 @@ const styles = StyleSheet.create({
   },
   icon: {marginLeft: 40},
   back: {marginTop: 30, marginLeft: 20},
-});
+})
